8ce1f1bb39adf084de9d3bec3e465b1237a14a38
Shilpi Karmakar <56488690+shilpikarmakar@users.noreply.github.com>
2022-07-08 19:47:36 +0530

    Create TransposeMatrix.java

---
+++ TransposeMatrix.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of columns: ");
+        int cols = sc.nextInt();
+        int matrix[][] = new int[rows][cols];
+        System.out.println("Enter the elements in the array: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        int arr[][] = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                arr[j][i] = matrix[i][j];                
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
